aa63f889f0c19d2a4d36f162d3d12fc46605afcc
sainarasimhankrishnamoorthy <sainarasimhan.krishnamoorthy@lloydsbanking.com>
2020-09-30 16:34:10 +0000

    Proto Changes

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #cmd to generate protobuf packages
-protoc --go_out=plugins=grpc:. ./pb/sample.proto ./pb/valid.proto
+#protoc --descriptor_set_out=sample.pb --go_out=plugins=grpc:. ./pb/sample.proto ./pb/valid.proto
+protoc --descriptor_set_out=pb/sample.pb --go_out=plugins=grpc:. ./pb/sample.proto 
 wire gen -output_file_prefix main- cmd/main.go
 
 
